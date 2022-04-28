ccfc15e4ca039ac82cb362de2b22fc9a0fd4db22
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-04-28 23:29:45 +0300

    initial

--- test/sanity.ts
+++ test/sanity.ts
@@ -1,5 +1,8 @@
+import { expect } from "chai";
+
 describe("sanity tests", async () => {
-  it("should work", async () => {
-    // expect(1+1)
+  it("should work with BigInt", async () => {
+    expect(1 + 1).eq(2);
+    expect(123456789123456789123456789123456789n + 1n).eq(BigInt("123456789123456789123456789123456790"));
   });
 });
