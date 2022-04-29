0ce349a7a6062856af3b5f7ad04997a36c06369b
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-04-29 14:09:16 +0300

    bn.js

--- test/sanity.ts
+++ test/sanity.ts
@@ -1,8 +1,16 @@
-import { expect } from "chai";
+import { use, expect } from "chai";
+import CBN from "chai-bn";
+import BN from "bn.js";
+
+use(CBN(BN)); // this adds .bignumber assertions
 
 describe("sanity tests", async () => {
   it("should work with BigInt", async () => {
     expect(1 + 1).eq(2);
     expect(123456789123456789123456789123456789n + 1n).eq(BigInt("123456789123456789123456789123456790"));
+  });
+
+  it("bn.js", async () => {
+    expect(new BN("123456789123456789123456789").addn(1)).bignumber.eq(new BN("123456789123456789123456790"));
   });
 });
